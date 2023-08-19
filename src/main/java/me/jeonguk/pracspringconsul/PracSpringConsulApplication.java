1e1bda1b6da765bb54586b66e3c9dec2783fbc6a
JeongUk Park <dnr1105@gmail.com>
2023-08-19 23:43:20 +0900

    init: init from start.spring.io with boot 3.0.9

---
+++ src/main/java/me/jeonguk/pracspringconsul/PracSpringConsulApplication.java
@@ -0,0 +1,13 @@
+package me.jeonguk.pracspringconsul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracSpringConsulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracSpringConsulApplication.class, args);
+	}
+
+}
